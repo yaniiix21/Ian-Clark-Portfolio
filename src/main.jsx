e528bd42106618079e9d51fcf91d6a4a9924f14e
ianClark <ivillegas@tekion.com>
2025-01-30 05:08:31 +0800

    Fixing production error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "@fontsource/outfit";
 import "@fontsource/roboto";
